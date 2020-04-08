476bc8cbb768d0e7e1d588e37042def3290394ac
mtakamur <s1820221@s.tsukuba.ac.jp>
2020-04-08 15:52:27 +0900

    scanf() avoided on MemoryAllocate.c

--- c/MemoryAllocate.c
+++ c/MemoryAllocate.c
@@ -20,16 +20,16 @@ int main(int argc, char *argv[])
     //////////////////////
     // Make first human //
     //////////////////////
-    humans = (human*)(malloc(sizeof(human)));
+    humans = (human *)(malloc(sizeof(human)));
 
     do
     {
         SetHuman(humans, counts);
         counts++;
-        humans = (human*)(realloc(humans, sizeof(human)*(counts+1)));
-    } while(AskNew());
+        humans = (human *)(realloc(humans, sizeof(human) * (counts + 1)));
+    } while (AskNew());
 
-    for(int i=0; i<counts; i++)
+    for (int i = 0; i < counts; i++)
     {
         ShowHuman(humans, i);
     }
@@ -43,16 +43,38 @@ int SetHuman(human *address, int human_no)
     char temp_name[32] = "\0";
     int temp_age = 0;
     char temp_gender = 'm';
+    char buf_name[32];// name buffer
+    char buf_age[32];// age buffer
+    char buf_gender[32];// gender buffer
+
     printf("/////////////////\n");
     printf("// Human No.%d //\n", human_no);
     printf("/////////////////\n");
     printf("Name > ");
     scanf("%s", temp_name);
     printf("Age > ");
+    //scanf("%d", &temp_age);
     scanf("%d%*c", &temp_age);
     printf("gender > ");
+    //scanf("%c", &temp_gender);
     scanf("%c%*c", &temp_gender);
-    
+
+    // Case fgets
+    //printf("/////////////////\n");
+    //printf("// Human No.%d //\n", human_no);
+    //printf("/////////////////\n");
+    //printf("Name > ");
+    //fgets(buf_name, sizeof(buf_name), stdin);
+    //strcpy(temp_name, buf_name);// Copy buffer to name
+
+    //printf("Age > ");
+    //fgets(buf_age, sizeof(buf_age), stdin);
+    //temp_age = atoi(buf_age);// Copy buffet to age
+
+    //printf("gender > ");
+    //fgets(buf_gender, sizeof(char), stdin);
+    //temp_gender = (char)(buf_gender[0]);
+
     //////////////////////////
     // Set human's property //
     //////////////////////////
@@ -77,13 +99,16 @@ bool AskNew()
 {
     bool FLAG = false;
     bool IsCORRECT = true;
+    char buffer[32];
     char answer;
 
+
+    /*Containing bug
     do
     {
         printf("Do you want to make new human? (y/n) >");
         scanf("%c", &answer);
-        printf("State of answer = %c", answer);
+        printf("State of answer = %c\n", answer);
         if(answer == 'y')
         {
             printf("y is selected\n");
@@ -100,5 +125,31 @@ bool AskNew()
             IsCORRECT = false;
         }
     } while (!IsCORRECT);
+    */
+
+    // Modified code
+    do
+    {
+        printf("Do you want to make new human? (y/n) >");
+        fgets(buffer, sizeof(buffer), stdin);
+        answer = (char)(buffer[0]);
+        if (answer == 'y')
+        {
+            printf("y is selected\n");
+            FLAG = true;
+            IsCORRECT = true;
+        }
+        else if (answer == 'n')
+        {
+            printf("n is selected\n");
+            FLAG = false;
+            IsCORRECT = true;
+        }
+        else
+        {
+            IsCORRECT = false;
+        }
+    } while (!IsCORRECT);
+
     return FLAG;
 }
