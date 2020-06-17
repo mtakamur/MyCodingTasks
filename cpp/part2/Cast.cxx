df6be50ee20a75667c5fc7020fc809ebb0559bcf
mtakamur <masahiro.takamura.alice@gmail.com>
2020-06-17 11:23:17 +0900

    cpp/Cast updated.
    Testing reinterpret_cast

--- cpp/part2/Cast.cxx
+++ cpp/part2/Cast.cxx
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-	int a = 5;
-	float b = 10.5;
+	int *a = new int(5);
+	float *b = new float(10.5);
 
-	a = static_cast<int>(b);
+	a = reinterpret_cast<int*>(b);
 
-	cout << "a = " << a << endl;
-	cout << "b = " << b << endl;
+	cout << "a = " << *a << endl;
+	cout << "b = " << *b << endl;
 	return 0;
 }
